c436ae9c58ed1960980633c911ce31acc174d14a
Lanmi5925 <32831183+Lanmi5925@users.noreply.github.com>
2018-05-09 01:18:57 +0800

    优化
    优化每次都要从第一章开始下载的问题

--- 04-qqComic.py
+++ 04-qqComic.py
@@ -10,10 +10,28 @@ import random
 import os
 
 
+def judge(n):
+	def panduan(n):
+		'''
+		判断要下载的章节是否存在
+		'''
+		path = "./Comic/episode" + str(n)
+		isExists = os.path.exists(path)
+		return isExists
+
+	while True:
+		if not panduan(n):
+			break 
+		else:
+			print("episode" + str(n) + " :已存在")
+			n += 1
+			panduan(n)
+	# 从第n章开始下载
+	return n
+
+
 def loadPage(url, i, n):
 	browser.get(url)
-	t = random.uniform(0.5, 0.6)
-	time.sleep(t)
 	# 先找到一幅未加载漫画的图片位置
 	targets = browser.find_elements_by_xpath('//div/ul/li[@style]/img[@src="//ac.gtimg.com/media/images/pixel.gif"]')
 	try:
@@ -29,40 +47,34 @@ def loadPage(url, i, n):
 		if len(targets) == 0:
 			break
 		target = targets[0]
-	time.sleep(t)
+	#time.sleep(0.5)
 	xpath = '//div/ul/li[@style]/img'
 	comicLinks = browser.find_elements_by_xpath(xpath)
 	path = "./Comic/episode" + str(n)
-	j = 1 # 用来给下载的每一章节的图片命名，计数
-	isExists = os.path.exists(path)
-	if not isExists:
-		try:
-			os.makedirs(path)
-		except:
-			print("error")
-			path = "./image/errorFile"
-		for element in comicLinks:
-			img_url = element.get_attribute('src')
-			#print(img_url)
-			writePage(img_url, j, path)
-			j += 1
-		print("----finished%d----"%n)
-	else:
-		print(path + " :已存在")
+	# 用来给下载的每一章节的图片命名，计数
+	j = 1 
+	os.makedirs(path)
+	for element in comicLinks:
+		img_url = element.get_attribute('src')
+		#print(img_url)
+		writePage(img_url, j, path)
+		j += 1
+	print("----finished%d----"%n)
 	n += 1
 	return n
 
 def dealPage(url):
+	# 处理图片链接
 	headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36'}
 	request = urllib.request.Request(url, headers=headers)
-	t = random.uniform(0.1, 0.3)
-	time.sleep(t)
+	# t = random.uniform(0.1, 0.3)
+	# time.sleep(t)
 	response = urllib.request.urlopen(request)
 	html = response.read()
 	return html
 
-def writePage(link, j, path):
-	image = dealPage(link)
+def writePage(url, j, path):
+	image = dealPage(url)
 
 	fileName = path + "/" + str(j) + ".jpg"
 	if os.path.exists(fileName) == False:
@@ -72,12 +84,13 @@ def writePage(link, j, path):
 if __name__ == '__main__':
 	browser = webdriver.PhantomJS()
 	browser.maximize_window()
-	i = 1
 	n = 1
 	latest = int(input("请输入海贼王最新话"))
+	# 如果之前已经下载过很多章，则继续往后下载
+	n = judge(n)
+	i = n
 	while n<=latest:
 		url = 'http://ac.qq.com/ComicView/index/id/505430/cid/' + str(i)
 		n = loadPage(url, i, n)
 		i += 1
-	
 	browser.quit()
